b6f6c88f1e11d36ead3da064e939b5702a97b9bc
agentz-shyam <shyam@agentz.ai>
2022-09-12 18:17:33 +0530

    test deploy 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello team, I have been deployed in AWS Amplify</h1>
+      <Typography variant="h1">Hello team, I have been deployed in AWS Amplify</Typography>
+      <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
     </div>
   );
 }
